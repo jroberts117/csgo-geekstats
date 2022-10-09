53f695ac5d044a4f19e69c150d1c15ef56014900
Mailb0xhead <wood.douglas@gmail.com>
2022-10-09 08:52:33 -0400

    geekstats v2.9.10.9.22 - API bug fixes
    Updated CSRF logic for ajax calls

--- geekstats/stats/static/scripts.js
+++ geekstats/stats/static/scripts.js
@@ -1,4 +1,19 @@
 
+		function getCookie(name) {
+			let cookieValue = null;
+			if (document.cookie && document.cookie !== "") {
+				const cookies = document.cookie.split(";");
+				for (let i = 0; i < cookies.length; i++) {
+				const cookie = cookies[i].trim();
+				// Does this cookie string begin with the name we want?
+				if (cookie.substring(0, name.length + 1) === (name + "=")) {
+					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+					break;
+				}
+				}
+			}
+			return cookieValue;
+			}
 
                 
     document.getElementById("rangeButton").onclick = function() {
@@ -36,28 +51,4 @@
         document.getElementById("graph").style['display'] = "none";
     };
 
-    function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== "") {
-          const cookies = document.cookie.split(";");
-          for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + "=")) {
-              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-              break;
-            }
-          }
-        }
-        return cookieValue;
-      }
-
-		$.ajaxSetup({
-			beforeSend: function(xhr, settings) {
-				if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-					// Only send the token to relative URLs i.e. locally.
-					xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-				}
-			}
-		});
 
